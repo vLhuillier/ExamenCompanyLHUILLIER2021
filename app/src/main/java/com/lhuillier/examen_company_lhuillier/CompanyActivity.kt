776105705fc920109473a00237a9defe8f65ae31
vlhuillier <vlhuillier@esimed.fr>
2021-01-26 10:10:35 +0100

    fix bugs master

--- app/src/main/java/com/lhuillier/examen_company_lhuillier/CompanyActivity.kt
+++ app/src/main/java/com/lhuillier/examen_company_lhuillier/CompanyActivity.kt
@@ -2,13 +2,14 @@ package com.lhuillier.examen_company_lhuillier
 
 import android.os.AsyncTask
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import com.lhuillier.examen_company_lhuillier.service.CompanyService
 import com.lhuillier.examen_lhuillier.data.model.Company
 import kotlinx.android.synthetic.main.content_company.*
@@ -48,40 +49,50 @@ class CompanyActivity : AppCompatActivity() {
             val textViewNic: TextView = findViewById<TextView>(R.id.nic_company) as TextView
             textViewNic.text = String.format(nicMessage, result?.nic)
 
-            /* Display L1 Normalisée */
-            val l1NormaliseeMessage = getString(R.string.l1_normalisee_company)
-            val textViewL1Normalisee: TextView = findViewById<TextView>(R.id.l1_normalisee_company) as TextView
-            textViewL1Normalisee.text = String.format(l1NormaliseeMessage, result?.l1_normalisee)
+            /* Display L1 Normalisized */
+            val l1NormalisizedMessage = getString(R.string.l1_normalisized_company)
+            val textViewL1Normalisized: TextView = findViewById<TextView>(R.id.l1_normalisized_company) as TextView
+            textViewL1Normalisized.text = String.format(l1NormalisizedMessage, result?.l1_normalisee)
 
-            /* Display L2 Normalisée */
-            val l2NormaliseeMessage = getString(R.string.l2_normalisee_company)
-            val textViewL2Normalisee: TextView = findViewById<TextView>(R.id.l2_normalisee_company) as TextView
-            textViewL2Normalisee.text = String.format(l2NormaliseeMessage, result?.l2_normalisee)
+            /* Display L2 Normalisized */
+            val l2NormalisizedMessage = getString(R.string.l2_normalisized_company)
+            val textViewL2Normalisized: TextView = findViewById<TextView>(R.id.l2_normalisized_company) as TextView
+            textViewL2Normalisized.text = String.format(l2NormalisizedMessage, result?.l2_normalisee)
 
-            /* Display L3 Normalisée */
-            val l3NormaliseeMessage = getString(R.string.l3_normalisee_company)
-            val textViewL3Normalisee: TextView = findViewById<TextView>(R.id.l3_normalisee_company) as TextView
-            textViewL3Normalisee.text = String.format(l3NormaliseeMessage, result?.l3_normalisee)
+            /* Display L3 Normalisized */
+            val l3NormalisizedMessage = getString(R.string.l3_normalisized_company)
+            val textViewL3Normalisized: TextView = findViewById<TextView>(R.id.l3_normalisized_company) as TextView
+            textViewL3Normalisized.text = String.format(l3NormalisizedMessage, result?.l3_normalisee)
 
-            /* Display L4 Normalisée */
-            val l4NormaliseeMessage = getString(R.string.l4_normalisee_company)
-            val textViewL4Normalisee: TextView = findViewById<TextView>(R.id.l4_normalisee_company) as TextView
-            textViewL4Normalisee.text = String.format(l4NormaliseeMessage, result?.l4_normalisee)
+            /* Display L4 Normalisized */
+            val l4NormalisizedMessage = getString(R.string.l4_normalisized_company)
+            val textViewL4Normalisized: TextView = findViewById<TextView>(R.id.l4_normalisized_company) as TextView
+            textViewL4Normalisized.text = String.format(l4NormalisizedMessage, result?.l4_normalisee)
 
-            /* Display L5 Normalisée */
-            val l5NormaliseeMessage = getString(R.string.l5_normalisee_company)
-            val textViewL5Normalisee: TextView = findViewById<TextView>(R.id.l5_normalisee_company) as TextView
-            textViewL5Normalisee.text = String.format(l5NormaliseeMessage, result?.l5_normalisee)
+            /* Display L5 Normalisized */
+            val l5NormalisizedMessage = getString(R.string.l5_normalisized_company)
+            val textViewL5Normalisized: TextView = findViewById<TextView>(R.id.l5_normalisized_company) as TextView
+            textViewL5Normalisized.text = String.format(l5NormalisizedMessage, result?.l5_normalisee)
 
-            /* Display L6 Normalisée */
-            val l6NormaliseeMessage = getString(R.string.l6_normalisee_company)
-            val textViewL6Normalisee: TextView = findViewById<TextView>(R.id.l6_normalisee_company) as TextView
-            textViewL6Normalisee.text = String.format(l6NormaliseeMessage, result?.l6_normalisee)
+            /* Display L6 Normalisized */
+            val l6NormalisizedMessage = getString(R.string.l6_normalisized_company)
+            val textViewL6Normalisized: TextView = findViewById<TextView>(R.id.l6_normalisized_company) as TextView
+            textViewL6Normalisized.text = String.format(l6NormalisizedMessage, result?.l6_normalisee)
 
-            /* Display L7 Normalisée */
-            val l7NormaliseeMessage = getString(R.string.l7_normalisee_company)
-            val textViewL7Normalisee: TextView = findViewById<TextView>(R.id.l7_normalisee_company) as TextView
-            textViewL7Normalisee.text = String.format(l7NormaliseeMessage, result?.l7_normalisee)
+            /* Display L7 Normalisized */
+            val l7NormalisizedMessage = getString(R.string.l7_normalisized_company)
+            val textViewL7Normalisized: TextView = findViewById<TextView>(R.id.l7_normalisized_company) as TextView
+            textViewL7Normalisized.text = String.format(l7NormalisizedMessage, result?.l7_normalisee)
+
+            /* Display label region */
+            val labelRegionMessage = getString(R.string.label_region_company)
+            val textViewLabelRegion: TextView = findViewById<TextView>(R.id.label_region_company) as TextView
+            textViewLabelRegion.text = String.format(labelRegionMessage, result?.libelle_region)
+
+            /* Display departement */
+            val departementMessage = getString(R.string.departement_company)
+            val textViewDepartement: TextView = findViewById<TextView>(R.id.departement_company) as TextView
+            textViewDepartement.text = String.format(departementMessage, result?.departement)
 
             progress.visibility = View.GONE
         }
@@ -99,7 +110,8 @@ class CompanyActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
         val textNameLocation = getString(R.string.toolbar_title_company)
         val nameLocation = company?.l1_normalisee
-        getSupportActionBar()?.setTitle(String.format(textNameLocation, nameLocation));
+        getSupportActionBar()?.setTitle(String.format(textNameLocation, nameLocation))
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
 
         val svc = CompanyService()
 
@@ -112,5 +124,19 @@ class CompanyActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return true
     }
 }
